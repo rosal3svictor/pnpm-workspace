0f7880da1fec9688c5bc84e9c962f4e1e11815dd
vrosalesmillan <victoreduardo.rosalesmillan@blackboard.com>
2022-09-21 22:41:54 -0500

    fix: GCXSEM-000 Enforce good practice with extra config

---
+++ packages/frontend-services/vite.config.ts
@@ -0,0 +1,37 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    // Vite plugin to help us generate the type definitions for our module.
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  root: "./",
+  publicDir: "src",
+  build: {
+    outDir: "lib",
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "@bb-platform-extensions/shared-components-frontend-services",
+      fileName: (format) =>
+        `@bb-platform-extensions/shared-components-frontend-services.${format}.js`,
+    },
+    rollupOptions: {
+      // Make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react-dom", "styled-components"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "styled-components": "styled",
+        },
+      },
+    },
+  },
+});
